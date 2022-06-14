126ea4aeab99e3c23cd54de8f027578034b01285
Rafael Rodriguez <rrrodz@sbcglobal.net>
2022-06-14 12:57:12 -0500

    lastcommit-614

---
+++ F-String.py
@@ -0,0 +1,10 @@
+name = "Rafael"
+age = 52
+print(f"hello {name}, you are {age}")
+
+name = 'dennis'
+age = 23
+
+print('%s is %d years old' % (name, age))
+print('{} is {} years old'.format(name, age))
+print(f'{name} is {age} years old')
