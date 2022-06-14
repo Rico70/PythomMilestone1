ca7b5ca6c6b9baf1a465371564d80d52b00c219b
Rafael Rodriguez <rrrodz@sbcglobal.net>
2022-06-14 11:38:18 -0500

    commit4

--- variables.py
+++ variables.py
@@ -5,4 +5,25 @@
 #print(str)
 
 # DataTypes
-a, b, c = 5, 6, 7
+#a, b, c = 5, 6, 7
+#print(a)
+#print(b)
+#print(c)
+
+# Python program to display numbers of
+# different data types
+
+# int
+num1 = 10
+num2 = 100
+print(num1+num2)
+
+# float
+a = 10.5
+b = 8.9
+print(a-b)
+
+# complex numbers
+x = 3 + 4j
+y = 9 + 8j
+print(y-x)
